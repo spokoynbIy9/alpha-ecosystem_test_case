dc0164440ac9128597085428d21f410a348931d3
Gusarov Anatoly <gusarov.tolia@mail.ru>
2024-09-07 22:06:57 +0500

    fix bug with favorite

--- src/pages/ProductsPage/components/ProductItem.tsx
+++ src/pages/ProductsPage/components/ProductItem.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Product } from "../../../types/products";
 import { useState } from "react";
 import useStyles from "../styles";
-import { useAppDispatch } from "../../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { addToFavorite, deleteProduct } from "../../../redux/productsSlice";
 import { useNavigate } from "react-router-dom";
 const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const [isFirstSvg, setIsFirstSvg] = useState(true);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const { ids } = useAppSelector((state) => state.products.favorites);
   const switchSvg = (event: React.MouseEvent) => {
     event.stopPropagation();
-    setIsFirstSvg((prevState) => !prevState);
+    setIsFavorite((prevState) => !prevState);
     dispatch(addToFavorite(product.id));
   };
   const handleDeleteProduct = (event: React.MouseEvent) => {
@@ -22,13 +23,16 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
     navigate(`/products/${product.id}`);
   };
   const classes = useStyles();
+  useEffect(() => {
+    setIsFavorite(ids.includes(product.id));
+  }, [product.id, ids]);
   return (
     <li className={classes.productElement} onClick={moveInfoPage}>
       <h3 className={classes.productTitle}>{product.title}</h3>
       <span className={classes.productDesc}>{product.description}</span>
       <div className={classes.buttonsWrapper}>
         <button onClick={switchSvg} className={classes.productButton}>
-          {isFirstSvg ? (
+          {isFavorite ? (
             <svg
               width="24"
               height="24"
@@ -37,8 +41,8 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M15.7 5C14.8 5 13 5.5 12 7.1C11 5.5 9.2 5 8.2 5C5.9 5 4 6.9 4 9.2C4 13.2 11.4 17.7 11.7 17.9L12 18.1L12.3 17.9C12.6 17.7 20 13.1 20 9.2C20 6.9 18.1 5 15.7 5ZM12 16.9C9.8 15.5 5 11.9 5 9.2C5 7.4 6.5 6 8.2 6C8.3 6 10.7 6.1 11.5 8.4L12 9.8L12.5 8.4C13.3 6.1 15.7 6 15.8 6C17.5 6 19 7.4 19 9.2C19 11.9 14.2 15.5 12 16.9Z"
-                fill="#1C2E45"
+                d="M16 5C16 5 13 5 12 8C11 5 8 5 8 5C5.8 5 4 6.8 4 9C4 13.1 12 18 12 18C12 18 20 13 20 9C20 6.8 18.2 5 16 5Z"
+                fill="#000ff0"
                 fillOpacity="0.6"
               />
             </svg>
@@ -51,8 +55,8 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M16 5C16 5 13 5 12 8C11 5 8 5 8 5C5.8 5 4 6.8 4 9C4 13.1 12 18 12 18C12 18 20 13 20 9C20 6.8 18.2 5 16 5Z"
-                fill="#000ff0"
+                d="M15.7 5C14.8 5 13 5.5 12 7.1C11 5.5 9.2 5 8.2 5C5.9 5 4 6.9 4 9.2C4 13.2 11.4 17.7 11.7 17.9L12 18.1L12.3 17.9C12.6 17.7 20 13.1 20 9.2C20 6.9 18.1 5 15.7 5ZM12 16.9C9.8 15.5 5 11.9 5 9.2C5 7.4 6.5 6 8.2 6C8.3 6 10.7 6.1 11.5 8.4L12 9.8L12.5 8.4C13.3 6.1 15.7 6 15.8 6C17.5 6 19 7.4 19 9.2C19 11.9 14.2 15.5 12 16.9Z"
+                fill="#1C2E45"
                 fillOpacity="0.6"
               />
             </svg>

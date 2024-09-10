5e7dbbcb52b4f173703001aadc50b0f5854e2ad8
Gusarov Anatoly <gusarov.tolia@mail.ru>
2024-09-10 08:51:08 +0500

    make so that priceRAngeSlider depend on prices products

--- src/pages/ProductsPage/components/PriceRangeSlider.tsx
+++ src/pages/ProductsPage/components/PriceRangeSlider.tsx
@@ -1,11 +1,18 @@
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { setPriceRange } from "../../../redux/productsSlice";
+import { useMemo } from "react";
 import Slider from "@mui/material/Slider";
 const PriceRangeSlider = () => {
   const dispatch = useAppDispatch();
+  const products = useAppSelector((state) => state.products.products);
   const priceRange = useAppSelector(
     (state) => state.products.filters.priceRange
   );
+  const [minPrice, maxPrice] = useMemo(() => {
+    if (products.length === 0) return [0, 100];
+    const prices = products.map((product) => product.price);
+    return [Math.min(...prices), Math.max(...prices)];
+  }, [products]);
   const handleSliderChange = (event: Event, value: number | number[]) => {
     if (Array.isArray(value)) {
       dispatch(setPriceRange({ min: value[0], max: value[1] }));
@@ -14,8 +21,8 @@ const PriceRangeSlider = () => {
 
   return (
     <Slider
-      min={0}
-      max={100}
+      min={minPrice}
+      max={maxPrice}
       value={[priceRange.min, priceRange.max]}
       step={1}
       valueLabelDisplay="auto"
